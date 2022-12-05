196173e52addf83e1b9fb96403e17a333483e380
Òscar Casajuana <elboletaire@gmail.com>
2022-12-05 19:18:07 +0100

    Remove (unnecessary) logo

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,9 +1,7 @@
 import { Code, Link, Text, VStack } from '@chakra-ui/react'
-import { Logo } from '../components/Logo'
 
 export const App = () => (
   <VStack spacing={8}>
-    <Logo h='40vmin' pointerEvents='none' />
     <Text>
       Edit <Code fontSize='xl'>src/App.tsx</Code> and save to reload.
     </Text>
