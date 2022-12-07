44cd819440c2b23a5ff0ba5aa6a67bbb94aebb92
Òscar Casajuana <elboletaire@gmail.com>
2022-12-07 20:17:59 +0100

    Fix location issue in test-utils

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,6 +1,6 @@
 // @ts-nocheck
 import { ChakraProvider, theme } from '@chakra-ui/react'
-import router from '@containers/router'
+import router from '@src/router'
 import { render, RenderOptions } from '@testing-library/react'
 import * as React from 'react'
 import { RouterProvider } from 'react-router-dom'
